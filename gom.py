7b59a5ec63a9f76999be246cc4ed3a45f47748fe
T-Sana <110158900+T-Sana@users.noreply.github.com>
2023-12-08 15:21:15 +0100

    Created UI

--- gom.py
+++ gom.py
@@ -1,6 +1,6 @@
 from Outils.cvt import *
 from Outils.cvt2 import *
-sz = [100, 100]
+sz = [123, 257]
 
 if sz[0]>sz[1]:
     max_x = 1000
@@ -36,6 +36,8 @@ if sz[0] <= 350 or sz[1] <= 350:
         for y in range2(0, max_y, max_y/sz[1]):
             img_life_g.rectangle([x, y], [x+max_x/sz[0], y+max_y/sz[1]], col.new('202020'), 1)
 jeu = ly.frame(img=copy.deepcopy(img_life), pos=[(1920-max_x)/2, 40])
+ui_img = image(img=image.new_img(dimensions=[380, 1000], fond=col.new('aaaaaa')))
+ui = ly.frame(img=copy.deepcopy(ui_img), pos=[40,40])
 
 voisins = [[-1, -1], [0, -1], [1, -1],
            [-1, 0], [1, 0],
@@ -53,12 +55,15 @@ def count_vsns(grille, pos) -> int:
         if grille[x_, y_] != 0:
             vsns += 1
     return vsns
+last = time.time()
 pause = False
 img_g = True
 while True:
+    gen = 0
     grille = np.full(sz, 0, np.int8)
     positions = []
-    for _ in range(rd.randint(0, sz[0]*sz[1]-1)):
+    cases = sz[0]*sz[1]-1
+    for _ in range(rd.randint(0, cases if cases < 100000 else 100000)):
         positions.append([rd.randint(0,sz[0]-1),rd.randint(0,sz[1]-1)])
     for p in positions:
         grille[p[0], p[1]] = 1
@@ -81,17 +86,27 @@ while True:
                 except:
                     pass
         jeu.img = img
+        ui_im = image(img=copy.deepcopy(ui_img))
+        ui_im.ecris(f'Generation: {gen:0>5}', [190, 50], police=cv2.FONT_HERSHEY_COMPLEX)
+        ui_im.ecris(f'Grille: {"on"if img_g else"off"}', [190, 100], police=cv2.FONT_HERSHEY_COMPLEX)
+        ui_im.ecris(f'Pause: {"oui"if pause else"non"}', [190, 150], police=cv2.FONT_HERSHEY_COMPLEX)
+        ui_im.ecris(f"Size: {sz[0]*sz[1]}", [190, 500], police=cv2.FONT_HERSHEY_COMPLEX)
+        now = time.time()
+        ui_im.ecris(f'{f"{1/diff(last,now):f}" if diff(last,now)>0.07 else f"{round(1/diff(last,now+0.000001)):0>3}"} FPS', [190, 920], police=cv2.FONT_HERSHEY_COMPLEX)
+        ui_im.ecris(f'{diff(last,now):f} SPF', [190, 970], police=cv2.FONT_HERSHEY_COMPLEX)
+        ui.img = ui_im
         img = ly.montre(True, col_dbg=col.new('202020'), attente=0)
         wk = souris_sur_image(img.img, souris, attente=1, destroy=non)
         match wk:
             case 27: raise SystemExit
             case 13: pause = not pause
-            case 8: grille = np.full(sz,0,np.int8)
+            case 8: grille = np.full(sz,0,np.int8); pause=True; gen=0
             case 32: img_g = not img_g
             case 114: break
             case -1: pass
             case _: print(wk)
         if not pause:
+            last = time.time()
             grille2 = np.full(sz, 0, np.int8)
             for x in range(sz[0]):
                 for y in range(sz[1]):
@@ -104,3 +119,5 @@ while True:
                         if n == 3:
                             grille2[x, y] = 1
             grille = copy.deepcopy(grille2)
+            gen += 1
+        else: last = time.time()
