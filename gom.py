e53bb6e69ab16a6f1719181b25e2928e561775d0
T-Sana <110158900+T-Sana@users.noreply.github.com>
2023-12-08 13:56:58 +0100

    Version 1.0.2

--- gom.py
+++ gom.py
@@ -1,5 +1,13 @@
 from Outils.cvt import *
 from Outils.cvt2 import *
+sz = [100, 100]
+
+if sz[0]>sz[1]:
+    max_x = 1000
+    max_y = 1000/sz[0]*sz[1]
+else:
+    max_y = 1000
+    max_x = 1000/sz[1]*sz[0]
 
 class ps:
     clck = False
@@ -10,24 +18,24 @@ def souris(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         ps.clck = True
         pos = [x, y]
-        if clicked_in(pos, [[(1920-1000)/2, 40], [1000+(1920-1000)/2, 1040]]):
-            ps.pos = [pos[n]-[(1920-1000)/2, 40][n] for n in [0,1]]
+        if clicked_in(pos, [[(1920-max_x)/2, 40], [max_x+(1920-max_x)/2, max_y+40]]):
+            ps.pos = [pos[n]-[(1920-max_x)/2, 40][n] for n in [0,1]]
     elif event == cv2.EVENT_MOUSEMOVE:
         if ps.clck and diff(ps.time, time.time()) > 0.1:
             pos = [x, y]
-            if clicked_in(pos, [[(1920-1000)/2, 40], [1000+(1920-1000)/2, 1040]]):
-                ps.pos = [pos[n]-[(1920-1000)/2, 40][n] for n in [0,1]]
+            if clicked_in(pos, [[(1920-max_x)/2, 40], [max_x+(1920-max_x)/2, max_y+40]]):
+                ps.pos = [pos[n]-[(1920-max_x)/2, 40][n] for n in [0,1]]
     elif event == cv2.EVENT_LBUTTONUP:
         ps.clck = False
 
-sz = [100, 100]
 ly = layout(img=image.new_img(fond=col.new('404040')))
-img_life = image(img=image.new_img(dimensions=[1000, 1000], fond=col.blanc))
+img_life = image(img=image.new_img(dimensions=[max_x, max_y], fond=col.blanc))
+img_life_g = copy.deepcopy(img_life)
 if sz[0] <= 350 or sz[1] <= 350:
-    for x in range(0, 1000, 1000//sz[0]):
-        for y in range(0, 1000, 1000//sz[1]):
-            img_life.rectangle([x, y], [x+1000//sz[0], y+1000//sz[1]], col.new('202020'), 1)
-jeu = ly.frame(img=copy.deepcopy(img_life), pos=[(1920-1000)/2, 40])
+    for x in range2(0, max_x, max_x/sz[0]):
+        for y in range2(0, max_y, max_y/sz[1]):
+            img_life_g.rectangle([x, y], [x+max_x/sz[0], y+max_y/sz[1]], col.new('202020'), 1)
+jeu = ly.frame(img=copy.deepcopy(img_life), pos=[(1920-max_x)/2, 40])
 
 voisins = [[-1, -1], [0, -1], [1, -1],
            [-1, 0], [1, 0],
@@ -46,6 +54,7 @@ def count_vsns(grille, pos) -> int:
             vsns += 1
     return vsns
 pause = False
+img_g = True
 while True:
     grille = np.full(sz, 0, np.int8)
     positions = []
@@ -56,26 +65,30 @@ while True:
     while True:
         if ps.pos != [0,0]:
             pos = ps.pos; ps.pos=[0,0]
-            x = round(pos[0]//(1000//sz[0]))
-            y = round(pos[1]//(1000//sz[1]))
+            x = round(pos[0]/(max_x/sz[0]))
+            y = round(pos[1]/(max_y/sz[1]))
             grille[x,y]=1 if grille[x,y]==0 else 0
-        img = copy.deepcopy(img_life)
-        for x in range(0, 1000, 1000//sz[0]):
-            for y in range(0, 1000, 1000//sz[1]):
+        if img_g:
+            img = copy.deepcopy(img_life_g)
+        else:
+            img = copy.deepcopy(img_life)
+        for x in range2(0, max_x, max_x/sz[0]):
+            for y in range2(0, max_y, max_y/sz[1]):
                 try:
-                    if grille[x//(1000//sz[0]), y//(1000//sz[1])] == 1:
-                        img.rectangle([x, y], [x+1000//sz[0], y +
-                                    1000//sz[1]], col.black, 0)
+                    if grille[round(x/(max_x/sz[0])), round(y/(max_y/sz[1]))] == 1:
+                        img.rectangle([x, y], [x+max_x/sz[0], y +
+                                    max_y/sz[1]], col.black, 0)
                 except:
                     pass
         jeu.img = img
         img = ly.montre(True, col_dbg=col.new('202020'), attente=0)
         wk = souris_sur_image(img.img, souris, attente=1, destroy=non)
         match wk:
-            case 32: break
             case 27: raise SystemExit
             case 13: pause = not pause
             case 8: grille = np.full(sz,0,np.int8)
+            case 32: img_g = not img_g
+            case 114: break
             case -1: pass
             case _: print(wk)
         if not pause:
