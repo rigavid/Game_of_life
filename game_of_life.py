539adb63e4dd76c49c812c4c98ffb85dbf82cb0c
Tim Tamet Martínez <11443315153@edu.gencat.cat>
2024-06-12 10:25:31 +0200

    E

--- game_of_life.py
+++ game_of_life.py
@@ -6,4 +6,4 @@ if True: ## Conway's game of life ##
     fenetre = layout(img=image.new_img(dimensions=[1920,1080,3]))
     matrix = np.full([1080,1080,3], col.black, np.uint8)
     jeu = fenetre.frame(pos=[(1920-1080)//2, 0], img=matrix, name='matrix')
-    fenetre.montre(except_frames=[jeu]).montre()
+    fenetre.montre(except_frames=[jeu])
